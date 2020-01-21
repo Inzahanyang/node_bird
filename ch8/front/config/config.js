914ad51afa733563db99ad30649c4c5d6efa6c80
inzahan <inzahanyang@gmail.com>
2020-01-21 18:02:30 +0900

    front/config

--- ch8/front/config/config.js
+++ ch8/front/config/config.js
@@ -1,3 +1,3 @@
-const backUrl = process.env.NODE_ENV === 'production' ? 'https://api.nodebird.com' : 'http://localhost:3065';
+const backUrl = process.env.NODE_ENV === "production" ? "https://13.124.176.57" : "http://localhost:3065";
 
 export { backUrl };
